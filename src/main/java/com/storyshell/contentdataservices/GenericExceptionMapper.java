7ae690d3ccdaaa48ca556eb5b48d46934bad099f
monarchpedo <rbosemonarch@gmail.com>
2017-07-20 02:06:05 +0530

    first commit for media and comment or message section srvices to promote the content driven system to the storyshell mechanism

---
+++ src/main/java/com/storyshell/contentdataservices/GenericExceptionMapper.java
@@ -0,0 +1,5 @@
+package com.storyshell.contentdataservices;
+
+public class GenericExceptionMapper {
+
+}
---
+++ src/main/java/com/storyshell/contentdataservices/JerseyConfig.java
@@ -0,0 +1,44 @@
+package com.storyshell.contentdataservices;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.boot.autoconfigure.jersey.JerseyProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.RequestContextFilter;
+
+@Configuration
+// @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+@EnableConfigurationProperties(JerseyProperties.class)
+@ApplicationPath("api/v1")
+public class JerseyConfig extends ResourceConfig {
+	@Inject
+	private JerseyProperties jersey;
+
+	public JerseyConfig() {
+
+	}
+
+	@PostConstruct
+	public void init() {
+		packages("com.storyshell.controller");
+		register(GenericExceptionMapper.class);
+		register(RequestContextFilter.class);
+	}
+
+	/*
+	 * @Bean
+	 * 
+	 * @ConditionalOnMissingBean public FilterRegistrationBean
+	 * requestContextFilter() { FilterRegistrationBean registration = new
+	 * FilterRegistrationBean(); //registration.setFilter(new
+	 * RequestContextFilter()); registration.setFilter(new ServletContainer());
+	 * //registration.addInitParameter("com.sun.jersey.config.property.packages"
+	 * ,""); //registration.setOrder(this.jersey.getFilter().getOrder() - 1);
+	 * //registration.setName("requestContextFilter"); return registration; }
+	 */
+
+}
